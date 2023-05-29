a73c19476b06a59c2eebefc87a46364541443e1b
HyeonHoo <easyhoho@naver.com>
2023-05-29 20:58:48 +0900

    네이버 open api 실행 도전

--- Movie_BoxOffice/ViewController.swift
+++ Movie_BoxOffice/ViewController.swift
@@ -6,36 +6,23 @@
 //
 import UIKit
 import Foundation
+import Alamofire // For network requests
 
-class Movie {
-    var link:String?
-    var imageURL:String?
-    var image:UIImage?
-    var userRating:String?
-
-    init() {
-
-    }
-}
-class MoviesTableViewController: UITableViewController, XMLParserDelegate{
-    let clientID        = "ctwIeRpCU4xcbGeFbhPP"    // ClientID
-    let clientSecret    = "bYHTkauZBG"              // ClientSecret
-
-    var movies:[Movie]      = []           // API를 통해 받아온 결과를 저장할 array
-
-    var strXMLData: String?         = ""   // xml 데이터를 저장
-    var currentTag: String?          = ""   // 현재 item의 element를 저장
-    var currentElement: String      = ""   // 현재 element의 내용을 저장
-
+// MARK: - MovieModel
+struct MovieModel: Codable {
+    let items: [NaverMovie]
 }
 
-struct NaverSearchResults: Codable {
-    let items: [NaverMovieItem]
+// MARK: - Item
+struct NaverMovie: Codable {
+    let title: String
+    let link: String
+    let image: String
+    //let subtitle, pubDate, director, actor: String
+    let userRating: String
 }
 
-struct NaverMovieItem: Codable {
-    let image: String?
-}
+
 
 struct MovieData: Codable {
     let boxOfficeResult: BoxOfficeResult
@@ -55,9 +42,9 @@ struct DailyBoxOfficeList: Codable {
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     var moviedata : MovieData?
+    var movieList: [NaverMovie] = []
     
     var movieURL  = "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=30e77e7642a595f8537d082d2de83fc2&targetDt=" //20230510
-    
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -67,45 +54,118 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return "박스오피스(영화진흥위원회제공: " + getYesterdayDate() + ")"
     }
+    var selectedMovieCode: String?
+    var selectedMovieName: String?
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
-        as! MyTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! MyTableViewCell
+        
         cell.Moviename.text =  moviedata?.boxOfficeResult.dailyBoxOfficeList[indexPath.row].movieNm
         
-                guard let Acc = moviedata?.boxOfficeResult.dailyBoxOfficeList[indexPath.row].audiAcc else {
-                    return cell
-                }
-                let numS = NumberFormatter()
-                numS.numberStyle = .decimal
-                guard let Accmulate = Int(Acc), let result = numS.string(for: Accmulate) else {
-                    return cell
-                }
-                cell.MovieAcc.text = "누적: \(result) 명"
+        guard let Acc = moviedata?.boxOfficeResult.dailyBoxOfficeList[indexPath.row].audiAcc else {
+            return cell
+        }
+        let numS = NumberFormatter()
+        numS.numberStyle = .decimal
+        guard let Accmulate = Int(Acc), let result = numS.string(for: Accmulate) else {
+            return cell
+        }
+        cell.MovieAcc.text = "누적: \(result) 명"
         
-                guard let openDt = moviedata?.boxOfficeResult.dailyBoxOfficeList[indexPath.row].openDt else {
-                    cell.OpenDt.text = nil
-                    return cell
-                }
-                
-                let formattedOpenDate = "개봉일: " + openDt
-                cell.OpenDt.text = formattedOpenDate
+        guard let openDt = moviedata?.boxOfficeResult.dailyBoxOfficeList[indexPath.row].openDt else {
+            cell.OpenDt.text = nil
+            return cell
+        }
         
-//        // 네이버 API에서 영화 이미지 가져오기
-//          guard let movieCode = moviedata?.boxOfficeResult.dailyBoxOfficeList[indexPath.row].movieCd else {
-//              return cell
-//          }
-          
-//          let naverAPIURL = "https://openapi.naver.com/v1/search/movie.json?query=\(movieCode)"
-//
-//          guard let naverURL = URL(string: naverAPIURL) else {
-//              return cell
-//          }
-        
-       
+        let formattedOpenDate = "개봉일: " + openDt
+        cell.OpenDt.text = formattedOpenDate
 
+        let MovieCode = moviedata?.boxOfficeResult.dailyBoxOfficeList[indexPath.row].movieCd
+        let MovieName =
+            moviedata?.boxOfficeResult.dailyBoxOfficeList[indexPath.row].movieNm
+        
+        selectedMovieCode = MovieCode
+        
+        selectedMovieName = MovieName
+        
+        // 영화 데이터를 가져와서 셀을 업데이트합니다.
+        getMovieData(for: cell, at: indexPath.row)
+        
         return cell
     }
-    
+
+    struct MovieService {
+        static let shared = MovieService()
+        let clientID = "ctwIeRpCU4xcbGeFbhPP"
+        let clientSecret = "5gHi0m1ON9"
+        let urlString = "https://openapi.naver.com/v1/search/movie.json"
+        
+        func fetchMovieData(movieCode: String, completion: @escaping (Result<Any, Error>) -> ()) {
+            let urlStr = urlString + "?query=\(movieCode)"
+            print(urlStr)
+            if let url = URL(string: urlStr) {
+                let session = URLSession(configuration: .default)
+                
+                var requestURL = URLRequest(url: url)
+                requestURL.addValue(clientID, forHTTPHeaderField: "X-Naver-Client-Id")
+                requestURL.addValue(clientSecret, forHTTPHeaderField: "X-Naver-Client-Secret")
+                
+                let dataTask = session.dataTask(with: requestURL) { (data, response, error) in
+                    if let error = error {
+                        print(error)
+                        return
+                    }
+                    
+                    if let safeData = data {
+                        print(String(data: safeData, encoding: .utf8)) // 응답 데이터 출력
+                        do {
+                            let decodedData = try JSONDecoder().decode(MovieModel.self, from: safeData)
+                            completion(.success(decodedData))
+                        } catch {
+                            print(error.localizedDescription)
+                        }
+                    } else {
+                        print("No data received")
+                        completion(.failure(NSError(domain: "No data received", code: -1, userInfo: nil)))
+                    }
+
+                }
+                dataTask.resume()
+            }
+        }
+    }
+
+    func getMovieData(for cell: MyTableViewCell?, at index: Int) {
+        guard let movieCode = selectedMovieCode else {
+            return
+        }
+        
+        MovieService.shared.fetchMovieData(movieCode: movieCode) { result in
+            switch result {
+            case .success(let movieData):
+                if let decodedData = movieData as? MovieModel {
+                    self.movieList = decodedData.items
+                    
+                    if let cell = cell, index < self.movieList.count {
+                        let userRating = self.movieList[index].userRating
+                        DispatchQueue.main.async {
+                            cell.MovieRt.text = "평점: \(userRating)"
+                        }
+                    }
+                    
+                    DispatchQueue.main.async {
+                        self.table.reloadData()
+                    }
+                    
+                    return
+                }
+            case .failure(let error):
+                print("fail", error)
+            }
+        }
+    }
+
+   
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -120,6 +180,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         
         movieURL += getYesterdayDate()
         getData()
+        getMovieData(for: nil, at: 0)
     }
     
     func getYesterdayDate() -> String {
@@ -130,22 +191,21 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         return dateFormatter.string(from: yesterday!) // 날짜를 지정된 형식으로 문자열로 변환하여 반환
     }
     
-    
     func getData() {
         guard let url = URL(string: movieURL) else {
-            print("Invalid URL")
+            print("잘못된 URL")
             return
         }
         
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: url) { data, response, error in
-            guard error == nil else {
-                print(error!)
+            if let error = error {
+                print("오류: \(error.localizedDescription)")
                 return
             }
             
             guard let JSONdata = data else {
-                print("No data received")
+                print("데이터를 받지 못했습니다")
                 return
             }
             
@@ -159,11 +219,12 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
                     self.table.reloadData()
                 }
             } catch {
-                print(error)
+                print("해독 오류: \(error)")
             }
         }
         
         task.resume()
     }
+
 }
 
