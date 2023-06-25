c7bb6bb0fd7700d78793eae5e4e17417f375de91
HyeonHoo <easyhoho@naver.com>
2023-06-25 21:26:17 +0900

    add Alamofire

--- Movie_BoxOffice/ViewController.swift
+++ Movie_BoxOffice/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by 신현호 on 2023/05/21.
 //
 import UIKit
+import Alamofire
 
 // MARK: - Model
 struct MovieData: Codable {
@@ -15,14 +16,14 @@ struct BoxOfficeResult: Codable {
     let dailyBoxOfficeList: [DailyBoxOfficeList]
 }
 
-struct DailyBoxOfficeList: Codable {
-    let movieNm : String
-    let audiAcc: String
-    let audiCnt: String
-    let openDt: String
-    let movieCd: String
-    let salesAcc: String
-    let rank: String
+struct DailyBoxOfficeList: Codable {    //사용 API 필드 구조
+    let movieNm : String    //영화이름
+    let audiAcc: String     //누적관객
+    let audiCnt: String     //해당일관객
+    let openDt: String      //개봉일
+    let movieCd: String     //영화코드
+    let salesAcc: String    //누적매출액
+    let rank: String        //영화 순위
 }
 
 // MARK: - ViewController
@@ -51,7 +52,7 @@ class ViewController: UIViewController {
         guard let dest = segue.destination as? DetailViewController else {
             return
         }
-
+        
         let myIndexPath = table.indexPathForSelectedRow!
         let row = myIndexPath.row
         dest.movieName = (moviedata?.boxOfficeResult.dailyBoxOfficeList[row].movieNm)!
@@ -72,95 +73,81 @@ class ViewController: UIViewController {
             return
         }
         
-        let session = URLSession(configuration: .default)
-        let task = session.dataTask(with: url) { data, response, error in
-            if let error = error {
-                print("오류: \(error.localizedDescription)")
-                return
-            }
-            
-            guard let JSONdata = data else {
-                print("데이터를 받지 못했습니다")
-                return
-            }
-            
-            _ = String(data: JSONdata, encoding: .utf8)
-            let decoder = JSONDecoder()
-            
-            do {
-                let decodedData = try decoder.decode(MovieData.self, from: JSONdata)
+        AF.request(url).validate().responseDecodable(of: MovieData.self) { response in
+            switch response.result {
+            case .success(let decodedData):
                 self.moviedata = decodedData
                 DispatchQueue.main.async {
                     self.table.reloadData()
                 }
-            } catch {
+            case .failure(let error):
                 print("해독 오류: \(error)")
             }
         }
-        
-        task.resume()
     }
 }
-
-// MARK: - UITableViewDataSource
-extension ViewController: UITableViewDataSource {
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! MyTableViewCell
-        // 데이터를 가져와 셀에 할당합니다.
-        guard let movieList = moviedata?.boxOfficeResult.dailyBoxOfficeList,
-              indexPath.row < movieList.count else {
+    // MARK: - UITableViewDataSource
+    extension ViewController: UITableViewDataSource {
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 10
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! MyTableViewCell
+            // 데이터를 가져와 셀에 할당합니다.
+            guard let movieList = moviedata?.boxOfficeResult.dailyBoxOfficeList,
+                  indexPath.row < movieList.count else {
+                return cell
+            }
+            
+            let movie = movieList[indexPath.row]
+            
+            cell.Moviename.text = movie.movieNm
+            cell.MovieRank.text = movie.rank + "."
+            
+            let numSa = NumberFormatter()
+            numSa.numberStyle = .decimal
+            if let salesAcc = Int(movie.salesAcc),
+               let result = numSa.string(for: salesAcc) {
+                cell.MovieSacc.text = "누적 매출액: \(result) 원"
+            }
+            
+            let numC = NumberFormatter()
+            numC.numberStyle = .decimal
+            if let audiCnt = Int(movie.audiCnt),
+               let result = numC.string(for: audiCnt) {
+                cell.MovieCnt.text = "오늘 관객수: \(result) 명"
+            }
+            
+            let numS = NumberFormatter()
+            numS.numberStyle = .decimal
+            if let audiAcc = Int(movie.audiAcc),
+               let result = numS.string(for: audiAcc) {
+                cell.MovieAcc.text = "누적 관객수: \(result) 명"
+            }
+            
+            let formattedOpenDate = "개봉일: " + movie.openDt
+            cell.OpenDt.text = formattedOpenDate
+            
             return cell
         }
-
-        let movie = movieList[indexPath.row]
-
-        cell.Moviename.text = movie.movieNm
-        cell.MovieRank.text = movie.rank + "."
-
-        let numSa = NumberFormatter()
-        numSa.numberStyle = .decimal
-        if let salesAcc = Int(movie.salesAcc),
-           let result = numSa.string(for: salesAcc) {
-            cell.MovieSacc.text = "누적 매출액: \(result) 원"
-        }
-
-        let numC = NumberFormatter()
-        numC.numberStyle = .decimal
-        if let audiCnt = Int(movie.audiCnt),
-           let result = numC.string(for: audiCnt) {
-            cell.MovieCnt.text = "오늘 관객수: \(result) 명"
-        }
-
-        let numS = NumberFormatter()
-        numS.numberStyle = .decimal
-        if let audiAcc = Int(movie.audiAcc),
-           let result = numS.string(for: audiAcc) {
-            cell.MovieAcc.text = "누적 관객수: \(result) 명"
-        }
-
-        let formattedOpenDate = "개봉일: " + movie.openDt
-        cell.OpenDt.text = formattedOpenDate
-
-        return cell
-    }
-}
-
-// MARK: - UITableViewDelegate
-extension ViewController: UITableViewDelegate {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "박스오피스(영화진흥위원회제공: " + getYesterdayDate() + ")"
     }
     
-    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
-        return "by Shin Hyeon Ho"
+    // MARK: - UITableViewDelegate
+    extension ViewController: UITableViewDelegate {
+        func numberOfSections(in tableView: UITableView) -> Int {
+            return 1
+        }
+        
+        func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+            return "박스오피스(영화진흥위원회제공: " + getYesterdayDate() + ")"
+        }
+        
+        func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+            return "by Shin Hyeon Ho"
+        }
     }
-}
+
